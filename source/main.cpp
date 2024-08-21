18a8f6d2d78526d53103030d73f952f0494e692b
pieshower <graysonmarks@gmail.com>
2024-08-21 02:04:04 -0400

    adding port open catch

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <serial/serial.h>
 
-// testing ssh again ugggh
+
+using namespace std;
+using namespace serial;
+
+string userPort_s = "/dev/ttyACM0";
+string userDara_s = "/dev/ttyACM2";
+
+int userPort_baud = 115200;
+int dataPort_baud = 921600;
 
 
 int main()
