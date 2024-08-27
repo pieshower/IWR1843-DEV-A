8b8ad3b9b37ecbe16ba19238d9997df068503ebe
pieshower <graysonmarks@gmail.com>
2024-08-26 23:45:11 -0400

    Got bytes in order. need to look further in doc to see if endianess is appropriate for all bytes.

--- source/main.cpp
+++ source/main.cpp
@@ -7,12 +7,10 @@
 
 
 std::string userPort_s = "/dev/ttyACM0";
-std::string dataPort_s = "/dev/ttyACM2";
+std::string dataPort_s = "/dev/ttyACM1";
 
 int userPort_baud = 115200;
 int dataPort_baud = 921600;
-
-
 
 int main() {
 
@@ -22,11 +20,11 @@ int main() {
     sleep(2);
 
     while (true) {
-        std::vector<uint16_t> data = myRadar.read();
 
+        std::vector<uint16_t> data = myRadar.read();
         std::cout << "data size: " << (int)data.size() << std::endl;
         std::cout << "data:";
-        for (unsigned long i = 0; i < data.size(); i++) {
+        for (auto i = 0; i < data.size(); i++) {
             std::cout << " " << std::hex << std::setw(4) << std::setfill('0') << data[i];
         }
         std::cout << std::endl << std::endl;
