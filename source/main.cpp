b69a38ab1cff54b6518d370b2ff02c0cd14fa4a8
pieshower <graysonmarks@gmail.com>
2024-09-17 07:26:20 -0400

    removing old visualizer methods

--- source/main.cpp
+++ source/main.cpp
@@ -2,19 +2,6 @@
 #include "../include/targetObject.h"
 #include "../include/visualizer.h"
 
-void addDetectedObjectsToPointCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, std::vector<detected_object_t> _detectedObjects) {
-    cloud->points.clear();
-    cloud->width = 15;
-    cloud->height = 1;
-    cloud->points.resize(cloud->width);
-
-    for (size_t i = 0; i < _detectedObjects.size(); ++i) {
-        cloud->points[i].x = _detectedObjects[i].x;
-        cloud->points[i].y = _detectedObjects[i].y;
-        cloud->points[i].z = _detectedObjects[i].z;
-    }
-}
-
 void radarLoop() {
     while (true) {
         if (mtx.try_lock()) {
@@ -32,40 +19,6 @@ void trackLoop() {
                 tracker.processDetectedObjects(detectedObjects);
             }
             mtx.unlock();
-        }
-    }
-}
-
-void visualizer() {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-  
-    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("PCL Visualizer"));
-    
-    viewer->setBackgroundColor(0.0, 0.0, 0.0);
-    viewer->addPointCloud<pcl::PointXYZ>(cloud);
-    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5);
-    viewer->addCoordinateSystem(5.0);
-
-    const double target_fps = 60;
-    const double frame_duration = 1.0 / target_fps;
-
-    while (!viewer->wasStopped()) {
-        auto start_time = std::chrono::high_resolution_clock::now();
-        
-        if (mtx.try_lock()) {
-            addDetectedObjectsToPointCloud(cloud, detectedObjects);
-            mtx.unlock();
-        }
-
-        viewer->updatePointCloud<pcl::PointXYZ>(cloud);     
-        viewer->spinOnce(1, true);
-
-        auto end_time = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed = end_time - start_time;
-
-        double time_to_sleep = frame_duration - elapsed.count();
-        if (time_to_sleep > 0) {
-            std::this_thread::sleep_for(std::chrono::duration<double>(time_to_sleep));
         }
     }
 }
