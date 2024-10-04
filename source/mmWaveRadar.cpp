66e7cf21e750d9e577bb7ec4ad574d7ba713b51e
pieshower <graysonmarks@gmail.com>
2024-10-03 22:41:27 -0400

    added a slightly better config, only sending detected objects now in config, and checking for if serial is available

--- source/mmWaveRadar.cpp
+++ source/mmWaveRadar.cpp
@@ -66,7 +66,7 @@ void mmWaveRadar::stop() {
 void mmWaveRadar::read() {
     std::vector<uint8_t> buf;
 
-    while (buf.size() < MAX_BUFFER_SIZE && dataPort.IsDataAvailable()) {
+    while (buf.size() < MAX_BUFFER_SIZE) {
         uint8_t byte;
         dataPort.ReadByte(byte);
         buf.push_back(byte);
