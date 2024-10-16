9ebdf4cb691025b4526ea5b144f07f76621bbd03
pieshower <graysonmarks@gmail.com>
2024-10-16 03:21:36 -0400

    adding servor inc and src

---
+++ include/servo.h
@@ -0,0 +1,35 @@
+#ifndef SERVO_H
+#define SERVO_H
+
+#include <gpiod.h>
+
+#include "../include/mmWaveRadar.h"
+
+#define PWM_FRQ 50
+
+#define MIN_DUT 13
+#define MAX_DUT 25
+
+#define MIN_ANG -90.0f
+#define MAX_ANG  90.0f
+
+#define MIN_RAD -1.0f
+#define MAX_RAD  1.0f
+
+class servo {
+private:
+    gpiod_line* servoLine;
+
+    uint8_t servoPin;
+    float currentAngle = 0;
+
+    float convertRadsToAngle(float _rads);
+
+public:
+     servo(int _pin);
+    ~servo();
+    
+    void setAngle();
+};
+
+#endif
---
+++ source/servo.cpp
@@ -0,0 +1,22 @@
+#include "../include/servo.h"
+#include "../include/mmWaveRadar_imp.h"
+
+const char* chip_s = "/dev/gpiochip0";
+gpiod_chip* chip = gpiod_chip_open(chip_s);
+
+servo::servo(int _pin) {
+    servoLine = gpiod_chip_get_line(chip, _pin);
+    servoPin = _pin;
+}
+
+servo::~servo() {
+    gpiod_line_release(servoLine);
+}
+
+float servo::convertRadsToAngle(float _rads) {
+    return (_rads - MIN_RAD) * (MAX_ANG - MIN_ANG) / (MAX_RAD - MIN_RAD) + MIN_ANG;
+}
+
+void servo::setAngle() {
+    std::cout << convertRadsToAngle(1.0) << std::endl;
+}
