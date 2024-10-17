d4414b7fc9667169d1dd75d97ee1006a9914ad66
pieshower <graysonmarks@gmail.com>
2024-10-17 06:32:06 -0400

    initial code for driving pwm

--- source/servo.cpp
+++ source/servo.cpp
@@ -38,7 +38,7 @@ void servo::pwmController() {
 
 void servo::setAngle(float &_rads) {
     dutyCylce_us = convertRadsToDutyCycle(_rads);
-    std::cout << "Duty Cylce: " << dutyCylce_us << std::endl;
+    std::cout << "Duty Cylce: " << dutyCylce_us << "\r";
     low_time = period_us - dutyCylce_us;
     high_time = period_us - low_time;
 }
