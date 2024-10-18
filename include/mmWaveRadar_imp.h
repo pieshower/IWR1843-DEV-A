8c235f8d4fbbcd46ae11ba3f34c87268807c2292
pieshower <graysonmarks@gmail.com>
2024-10-17 20:29:11 -0400

    servo class working great

--- include/mmWaveRadar_imp.h
+++ include/mmWaveRadar_imp.h
@@ -71,7 +71,7 @@ extern std::mutex mtx;
 
 float PackRGB(uint8_t r, uint8_t g, uint8_t b);
 
-extern const char* chip_s = "/dev/gpiochip0";
-extern gpiod_chip* chip = gpiod_chip_open(chip_s);
+extern const char* chip_s;
+extern gpiod_chip* chip;
 
 #endif
