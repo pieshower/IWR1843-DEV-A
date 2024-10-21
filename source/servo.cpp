8af6253c4848447a2070f7fe1f61b51553a27d7f
pieshower <graysonmarks@gmail.com>
2024-10-21 01:05:20 -0400

    testing servo with object data

--- source/servo.cpp
+++ source/servo.cpp
@@ -18,7 +18,7 @@ servo::servo(uint _pin, uint _frequency, gpiod_chip *_chip) {
     period_us = 1000000 / _frequency;
     active = true;
     servoThread = std::thread([this](){this->pwmController();});
-    servoThread.join();
+    servoThread.detach();
 }
 
 servo::~servo() {
